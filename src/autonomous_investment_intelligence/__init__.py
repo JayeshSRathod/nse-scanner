a68cf0fb263b23a8b6322ebe320c812f81ab311d
Jayesh Rathod <152681955+JayeshSRathod@users.noreply.github.com>
2026-07-31 01:29:27 +0530

    Sprint 15: add autonomous intelligence package

---
+++ src/autonomous_investment_intelligence/__init__.py
@@ -0,0 +1,13 @@
+"""Sprint 15 Autonomous Investment Intelligence contracts."""
+
+from .models import (
+    AutonomousDecisionCycle,
+    AutonomousDecisionOutcome,
+    HumanApproval,
+)
+
+__all__ = [
+    "AutonomousDecisionCycle",
+    "AutonomousDecisionOutcome",
+    "HumanApproval",
+]
